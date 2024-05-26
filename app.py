1677135d6a52357584a739fdc8bf2e02181a790c
Nihal Chahal <nchahal2002@gmail.com>
2024-05-25 22:01:04 -0500

    made changes to file

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
-# config
-from flask import Flask # type: ignore
-app = Flask(__name__)
-
-# index route
-@app.route('/')
-def index():
-    return 'Hello, this is PetFax!'
+from petfax import create_app
+app = create_app()
